6020d4c922fb13a70703154ee741bb26d0856773
Runemoro <runemoro@live.ca>
2018-05-20 23:24:17 -0400

    Fix fire animation not being ticked for entites on fire

---
+++ src/main/java/org/dimdev/vanillafix/textures/mixins/client/MixinRender.java
@@ -0,0 +1,25 @@
+package org.dimdev.vanillafix.textures.mixins.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.entity.Entity;
+import org.dimdev.vanillafix.textures.TemporaryStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Render.class)
+public class MixinRender {
+    /**
+     * @reason Adds the fire texture to the list of textures used this
+     * tick such that it can be animated next tick.
+     */
+    @Inject(method = "renderEntityOnFire", at = @At("HEAD"))
+    private void addFireTextureOnRenderEntityOnFire(Entity entity, double x, double y, double z, float partialTicks, CallbackInfo ci) {
+        TextureMap textureMapBlocks = Minecraft.getMinecraft().getTextureMapBlocks();
+        TemporaryStorage.texturesUsed.add(textureMapBlocks.getAtlasSprite("minecraft:blocks/fire_layer_0"));
+        TemporaryStorage.texturesUsed.add(textureMapBlocks.getAtlasSprite("minecraft:blocks/fire_layer_1"));
+    }
+}
