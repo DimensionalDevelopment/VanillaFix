38fb47426d2fb61eacd868ff6a3a2a492b93964b
Runemoro <runemoro@live.ca>
2018-06-01 14:35:11 -0400

    Disable anticheat

--- src/main/java/org/dimdev/vanillafix/bugs/mixins/MixinNetHandlerPlayServer.java
+++ src/main/java/org/dimdev/vanillafix/bugs/mixins/MixinNetHandlerPlayServer.java
@@ -24,7 +24,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Set;
 
 @Mixin(value = NetHandlerPlayServer.class, priority = 500) // Sponge
 public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer {
@@ -40,19 +39,6 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
     @Shadow private static boolean isMovePlayerPacketInvalid(CPacketPlayer packetIn) { return false; }
     @Shadow private void captureCurrentPosition() {}
 
-    private static final boolean SKIP_ANTICHEAT = true;
-    private static final boolean DEBUG_SPEED_BUFFER = false;
-    private static final double EXCESS_SPEED_BUFFER = 3; // blocks (balance between resync packets and client desyncs)
-    private static final double SPEED_MULTIPLIER = 0.11785905161311232 / 0.1;
-
-    private double horizontalDistance = 0;
-    private double verticalDistance = 0;
-    private double queuedX = 0;
-    private double queuedY = 0;
-    private double queuedZ = 0;
-    private long lastTickTime = -1;
-    private double tickLag = 1;
-
     /**
      * @reason See https://github.com/DimensionalDevelopment/VanillaFix/wiki/Move-Logic-Rewrite
      * <p>
@@ -66,6 +52,7 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
      * - lastGoodX/Y/Z not captured after teleport
      * - Redundant code in processPlayer
      * - Implement a better anticheat system (don't trust the client about anything)
+     * - TODO: Walking against flowing water does not add the correct movement stats
      */
     @Overwrite
     @Override
@@ -125,99 +112,15 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
         // Handle jump
         if (player.onGround && !packet.isOnGround() && yDiff > 0.0D) {
             player.jump();
+            player.posY += 0.5;
         }
 
-        if (!player.capabilities.isFlying && !packet.isOnGround()) {
-            yDiff = 0;
-            packetY = player.posY;
-        }
-
-        boolean trustPlayer = SKIP_ANTICHEAT || server.isSinglePlayer() && server.getServerOwner().equals(player.getName());
-        if (trustPlayer) {
-            double prevX = player.posX;
-            double prevY = player.posY;
-            double prevZ = player.posZ;
-            player.move(MoverType.PLAYER, xDiff, yDiff, zDiff);
-
-            // Move caused a teleport, capture new position, and stop processing packet
-            if (targetPos != null) {
-                captureCurrentPosition();
-                server.getPlayerList().serverUpdateMovingPlayer(player);
-                return;
-            }
-
-            player.setPositionAndRotation(packetX, packetY, packetZ, packetYaw, packetPitch);
-            player.addMovementStat(player.posX - prevX, player.posY - prevY, player.posZ - prevZ);
-            server.getPlayerList().serverUpdateMovingPlayer(player);
-            return;
-        }
-
-        // TODO: Fix everything below this line
-
-        if (!packet.isOnGround() && !player.capabilities.isFlying) {
+        if (!player.capabilities.isFlying && !player.isOnLadder() && !player.isInWater() && !player.onGround && !packet.isOnGround() && player.motionY <= -0.5) {
             yDiff = 0;
         }
-
-        // Check that a movement with a change in y is valid
-        if (yDiff < 0.01 && yDiff > -0.01) yDiff = 0; // Just caused by client/server not being in perfect sync, ignore
-        if (yDiff != 0 && !player.capabilities.isFlying) {
-            if (yDiff > player.stepHeight || yDiff < -player.stepHeight) {
-                LOGGER.warn("{} tried to step over a step too high! yDiff = {}", player.getName(), yDiff);
-                yDiff = 0;
-            } else if (player.onGround) {
-                AxisAlignedBB newBoundingBox = player.getEntityBoundingBox().offset(xDiff, yDiff, zDiff);
-                boolean targetOnGround = !player.world.getCollisionBoxes(player, newBoundingBox.expand(0, -0.05, 0)).isEmpty();
-                if (!targetOnGround) {
-                    LOGGER.warn("{} tried to fly! yDiff = {}", player.getName(), yDiff);
-                    yDiff = 0;
-                }
-                // TODO: Make sure the player didn't walk over gaps
-            } else {
-                if (yDiff < -1 || yDiff > 1) {
-                    LOGGER.warn("{} tried to control y position while falling! yDiff = {}", player.getName(), yDiff);
-                }
-                yDiff = 0;
-            }
+        if (yDiff > 0) {
+            player.fallDistance = 0;
         }
-
-        // Check that max movement speed is being respected
-        double hDiff = Math.sqrt(xDiff * xDiff + zDiff * zDiff);
-        double vDiff = yDiff > 0 ? yDiff : -yDiff;
-        double maxSpeed = SPEED_MULTIPLIER * tickLag * (player.capabilities.isFlying ? 3 * player.capabilities.getFlySpeed() : player.capabilities.getWalkSpeed());
-        if (player.isSneaking()) maxSpeed *= 0.3;
-        if (player.isSprinting()) maxSpeed *= 1.3;
-
-        if (horizontalDistance + hDiff > maxSpeed) {
-            boolean buffer;
-            if (horizontalDistance + hDiff - maxSpeed > EXCESS_SPEED_BUFFER) {
-                LOGGER.warn("{} moved too quickly! hDiff = {}, maxSpeed = {}", player.getName(), hDiff, maxSpeed);
-                syncClientPosition();
-                return;
-            } else {
-                if (DEBUG_SPEED_BUFFER) LOGGER.info("{} has excess speed, buffering! horizontalDistance = {}, maxSpeed = {}", player.getName(), hDiff, maxSpeed);
-                buffer = true;
-            }
-            double prevXDiff = xDiff;
-            double prevZDiff = zDiff;
-
-            hDiff = maxSpeed - horizontalDistance;
-
-            double angle = Math.atan2(zDiff, xDiff);
-            xDiff = hDiff * Math.cos(angle);
-            zDiff = hDiff * Math.sin(angle);
-
-            if (buffer) { // TODO: queue the actual movement path, not just displacement
-                queuedX = prevXDiff - xDiff;
-                queuedZ = prevZDiff - zDiff;
-            }
-        } else {
-            queuedX = 0;
-            queuedZ = 0;
-        }
-
-        horizontalDistance += hDiff;
-
-        // TODO: check vertical speed
 
         // Store info about the old position
         double prevX = player.posX;
@@ -236,10 +139,10 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
         }
 
         // Check if we need to resync the player (they moved wrongly), or accept the packet's position
-        double xError = packetX - (player.posX + queuedX);
-        double yError = packetY - (player.posY + queuedY);
-        double zError = packetZ - (player.posZ + queuedZ);
-        double errorSq = xError * xError /*+ yError * yError */ + zError * zError;  // TODO: Figure out why y-desync happens (client-side bug)
+        double xError = packetX - player.posX;
+        double yError = packetY - player.posY;
+        double zError = packetZ - player.posZ;
+        double errorSq = xError * xError + zError * zError; // TODO
 
         // The player should be allowed to move out of blocks, but not into blocks other than the
         // ones they're currently in.
@@ -254,7 +157,7 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
         }
 
         // Accept the packet's position if it's close enough (0.25 blocks) and not in a new block
-        if (errorSq > 0.0625D || yError > 10 || yError < -10) {
+        if (errorSq > 0.0625D || yError > 5 || yError < -5) {
             LOGGER.warn("{} out of sync, resyncing! error = {}", player.getName(), Math.sqrt(errorSq));
             player.setPositionAndRotation(player.posX, player.posY, player.posZ, packetYaw, packetPitch);
             syncClientPosition();
@@ -263,8 +166,6 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
             player.setPositionAndRotation(player.posX, player.posY, player.posZ, packetYaw, packetPitch);
             syncClientPosition();
         } else {
-            // TODO: Isn't the tolerance of 0.25 blocks too big? Couldn't the player pass through a glass pane?
-            //player.setPositionAndRotation(prevX + xDiff, prevY + yDiff, prevZ + zDiff, packetYaw, packetPitch);
             player.setPositionAndRotation(player.posX, player.posY, player.posZ, packetYaw, packetPitch);
         }
 
@@ -276,7 +177,6 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
 
     /** Resyncs the client's position (but not rotation, to make it less annoying for the player) **/
     private void syncClientPosition() {
-        queuedX = queuedY = queuedZ = 0;
         targetPos = new Vec3d(player.posX, player.posY, player.posZ);
         player.connection.sendPacket(new SPacketPlayerPosLook(
                 player.posX,
@@ -286,13 +186,6 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
                 0,
                 EnumSet.of(SPacketPlayerPosLook.EnumFlags.X_ROT, SPacketPlayerPosLook.EnumFlags.Y_ROT),
                 ++teleportId == Integer.MAX_VALUE ? 0 : teleportId));
-    }
-
-    @Inject(method = "setPlayerLocation(DDDFFLjava/util/Set;)V", at = @At("HEAD"))
-    private void resetQueuedMovement(double x, double y, double z, float yaw, float pitch, Set<SPacketPlayerPosLook.EnumFlags> relativeSet, CallbackInfo ci) {
-        if (!relativeSet.contains(SPacketPlayerPosLook.EnumFlags.X)) queuedX = 0;
-        if (!relativeSet.contains(SPacketPlayerPosLook.EnumFlags.Y)) queuedY = 0;
-        if (!relativeSet.contains(SPacketPlayerPosLook.EnumFlags.Z)) queuedZ = 0;
     }
 
     /**
@@ -322,26 +215,5 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
     @Inject(method = "update", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/entity/player/EntityPlayerMP;onUpdateEntity()V", ordinal = 0))
     private void afterUpdateEntity(CallbackInfo ci) {
         captureCurrentPosition();
-    }
-
-    /**
-     * @reason Reset distance travelled in a tick every tick. A teleport does not
-     * reset this, and this is intended.
-     */
-    @Inject(method = "update", at = @At("HEAD"))
-    private void resetDistanceTravelled(CallbackInfo ci) {
-        long time = System.currentTimeMillis();
-        if (lastTickTime != -1) tickLag = ((time - lastTickTime) / 1000D) * 20;
-        lastTickTime = time;
-
-        horizontalDistance = 0;
-        verticalDistance = 0;
-        if (queuedX != 0 || queuedY != 0 || queuedZ != 0) {
-            // TODO: don't make a fake packet, instead split processPlayer into two methods
-            processPlayer(new CPacketPlayer.Position(player.posX + queuedX, player.posY + queuedY, player.posZ + queuedZ, player.onGround));
-            if (DEBUG_SPEED_BUFFER && queuedX == 0 && queuedY == 0 && queuedZ == 0) {
-                LOGGER.info("{}'s speed buffer cleared!", player.getName());
-            }
-        }
     }
 }
