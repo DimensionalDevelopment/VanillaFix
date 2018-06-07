a49ed36cf99afe40008e023673fbc82242059c2f
Runemoro <runemoro@live.ca>
2018-06-06 22:49:55 -0400

    Add support for Tinker's animated fluids

---
+++ src/main/java/org/dimdev/vanillafix/textures/modsupport/mixins/client/MixinGuiUtil.java
@@ -0,0 +1,19 @@
+package org.dimdev.vanillafix.textures.modsupport.mixins.client;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import org.dimdev.vanillafix.textures.IPatchedTextureAtlasSprite;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import slimeknights.tconstruct.library.client.GuiUtil;
+
+@Pseudo
+@Mixin(GuiUtil.class)
+public class MixinGuiUtil {
+    @Inject(method = "renderTiledTextureAtlas", at = @At("HEAD"))
+    private static void onRenderTiledTextureAtlas(int x, int y, int width, int height, float depth, TextureAtlasSprite sprite, boolean upsideDown, CallbackInfo ci) {
+        ((IPatchedTextureAtlasSprite) sprite).markNeedsAnimationUpdate();
+    }
+}
---
+++ src/main/java/org/dimdev/vanillafix/textures/modsupport/mixins/client/MixinRenderUtil.java
@@ -0,0 +1,26 @@
+package org.dimdev.vanillafix.textures.modsupport.mixins.client;
+
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
+import org.dimdev.vanillafix.textures.IPatchedTextureAtlasSprite;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import slimeknights.tconstruct.library.client.RenderUtil;
+
+@Pseudo
+@Mixin(RenderUtil.class)
+public class MixinRenderUtil {
+    @Inject(method = "putTexturedQuad(Lnet/minecraft/client/renderer/BufferBuilder;Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;DDDDDDLnet/minecraft/util/EnumFacing;IIIIIIZZ)V", at = @At("HEAD"))
+    private static void onRenderQuad(BufferBuilder renderer, TextureAtlasSprite sprite, double x, double y, double z, double w, double h, double d, EnumFacing face, int r, int g, int b, int a, int light1, int light2, boolean flowing, boolean flipHorizontally, CallbackInfo ci) {
+        ((IPatchedTextureAtlasSprite) sprite).markNeedsAnimationUpdate();
+    }
+
+    @Inject(method = "putRotatedQuad(Lnet/minecraft/client/renderer/BufferBuilder;Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;DDDDDLnet/minecraft/util/EnumFacing;IIIIIIZ)V", at = @At("HEAD"))
+    private static void onRenderRotatedQuad(BufferBuilder renderer, TextureAtlasSprite sprite, double x, double y, double z, double w, double d, EnumFacing rotation, int r, int g, int b, int a, int light1, int light2, boolean flowing, CallbackInfo ci) {
+        ((IPatchedTextureAtlasSprite) sprite).markNeedsAnimationUpdate();
+    }
+}
