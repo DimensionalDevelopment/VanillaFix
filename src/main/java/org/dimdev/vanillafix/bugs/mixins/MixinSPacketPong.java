7c19346660b38efbb2ce1b2e73407fb87033d31a
creeper123123321 <creeper123123321@gmail.com>
2018-07-12 16:46:19 -0300

    private field

--- src/main/java/org/dimdev/vanillafix/bugs/mixins/MixinSPacketPong.java
+++ src/main/java/org/dimdev/vanillafix/bugs/mixins/MixinSPacketPong.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(SPacketPong.class)
 public class MixinSPacketPong implements IPatchedSPacketPong {
-    @Shadow long clientTime;
+    @Shadow private long clientTime;
     @Override
     public long getClientTime() {
         return clientTime;
