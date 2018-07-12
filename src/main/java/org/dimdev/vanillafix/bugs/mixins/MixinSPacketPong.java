f0a046fdb4398315a71ec2508ce9bb2a2d977149
creeper123123321 <creeper123123321@gmail.com>
2018-07-12 16:54:42 -0300

    Fix ping exploit

---
+++ src/main/java/org/dimdev/vanillafix/bugs/mixins/MixinSPacketPong.java
@@ -0,0 +1,15 @@
+package org.dimdev.vanillafix.bugs.mixins;
+
+import net.minecraft.network.status.server.SPacketPong;
+import org.dimdev.vanillafix.bugs.IPatchedSPacketPong;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(SPacketPong.class)
+public class MixinSPacketPong implements IPatchedSPacketPong {
+    @Shadow private long clientTime;
+    @Override
+    public long getClientTime() {
+        return clientTime;
+    }
+}
