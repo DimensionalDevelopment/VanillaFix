f9099ac271a9dd4887e3d0586d52e65268e96bb2
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-07 20:25:20 +0530

    Apply mixin only on client
     Changes to be committed:
    	modified:   src/main/java/org/dimdev/vanillafix/textures/mixins/SpriteMixin.java
    	modified:   src/main/resources/vanillafix.textures.mixins.json

--- src/main/java/org/dimdev/vanillafix/textures/mixins/SpriteMixin.java
+++ src/main/java/org/dimdev/vanillafix/textures/mixins/SpriteMixin.java
@@ -6,6 +6,10 @@ import org.spongepowered.asm.mixin.Mixin;
 
 import net.minecraft.client.texture.Sprite;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
 @MixinConfigValue(category = "clientOnly", key = "optimizedAnimatedTextures")
 @Mixin(Sprite.class)
 public class SpriteMixin implements SpriteExtensions {
