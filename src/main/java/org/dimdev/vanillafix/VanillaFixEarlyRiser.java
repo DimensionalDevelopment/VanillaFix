3a54260bcc971dd505aab7d3d2af391cc2ee4b86
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-15 11:46:35 +0530

    Log a message
     Changes to be committed:
    	modified:   src/main/java/org/dimdev/vanillafix/VanillaFixEarlyRiser.java

--- src/main/java/org/dimdev/vanillafix/VanillaFixEarlyRiser.java
+++ src/main/java/org/dimdev/vanillafix/VanillaFixEarlyRiser.java
@@ -6,6 +6,7 @@ public class VanillaFixEarlyRiser implements Runnable {
     @Override
     public void run() {
         if (VanillaFix.CONFIG.clientOnly.optimizedAnimatedTextures) {
+            VanillaFix.LOGGER.debug("Registering Animated Texture Optimization Mixins");
             Mixins.addConfiguration("vanillafix.textures.mixins.json");
         }
     }
