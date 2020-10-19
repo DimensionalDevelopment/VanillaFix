fbaebae4587091cf6e046f8a9134e5b1de206508
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-19 17:10:03 +0530

    Fix compatibility with sodium
     Changes to be committed:
    	modified:   src/main/java/org/dimdev/vanillafix/VanillaFixEarlyRiser.java

--- src/main/java/org/dimdev/vanillafix/VanillaFixEarlyRiser.java
+++ src/main/java/org/dimdev/vanillafix/VanillaFixEarlyRiser.java
@@ -2,10 +2,14 @@ package org.dimdev.vanillafix;
 
 import org.spongepowered.asm.mixin.Mixins;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 public class VanillaFixEarlyRiser implements Runnable {
     @Override
     public void run() {
-        if (VanillaFix.config().clientOnly.optimizedAnimatedTextures) {
+        if (VanillaFix.config().clientOnly.optimizedAnimatedTextures ||
+                // sodium already does this
+                !(FabricLoader.getInstance().isModLoaded("sodium"))) {
             VanillaFix.LOGGER.info("Registering Animated Texture Optimization Mixins");
             Mixins.addConfiguration("vanillafix.textures.mixins.json");
         }
