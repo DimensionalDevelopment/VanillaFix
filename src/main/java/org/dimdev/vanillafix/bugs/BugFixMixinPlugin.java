3bf5cfed30f13d0063feba7c0e6c043625524a5a
SD <harrypotter.starwars.shrish@gmail.com>
2020-12-28 17:14:23 +0530

    Epic new config

--- src/main/java/org/dimdev/vanillafix/bugs/BugFixMixinPlugin.java
+++ src/main/java/org/dimdev/vanillafix/bugs/BugFixMixinPlugin.java
@@ -1,8 +1,10 @@
 package org.dimdev.vanillafix.bugs;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
+import org.dimdev.vanillafix.VanillaFix;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -10,60 +12,44 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import static org.dimdev.vanillafix.VanillaFix.config;
 
 public class BugFixMixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+	}
 
-    @Override
-    public void onLoad(String mixinPackage) {
-    }
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
 
-    @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return Optional.ofNullable(VanillaFix.MIXIN_CONFIGS.get(targetClassName)).map(pair -> {
+			try {
+				Object e = VanillaFix.config()
+						.getClass()
+						.getField(pair.getCategory())
+						.get(VanillaFix.config());
+				return e.getClass().getField(pair.getValue()).getBoolean(e);
+			} catch (IllegalAccessException | NoSuchFieldException ignored) {
+				throw new AssertionError();
+			}
+		}).orElse(Boolean.TRUE);
+	}
 
-    // TODO: use asm
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        System.out.println(mixinClassName);
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
 
-        switch (mixinClassName) {
-            case "org.dimdev.vanillafix.bugs.mixins.step.ServerPlayerEntityMixin":
-                return config().antiCheat.fixStepHeight;
-            case "org.dimdev.vanillafix.bugs.mixins.EntityMixin":
-                return config().bugFixes.updateFallDistance;
-            case "org.dimdev.vanillafix.bugs.mixins.MinecraftServerMixin":
-                return config().bugFixes.disableInitialChunkLoad;
-            case "org.dimdev.vanillafix.bugs.mixins.PlayerInventoryMixin":
-                return config().bugFixes.fixRecipeBookIngredientsWithTags;
-            case "org.dimdev.vanillafix.bugs.mixins.invulnerable.ServerPlayerEntityMixin":
-                return config().antiCheat.noPlayerInvulnerabilityAfterTeleport;
-            case "org.dimdev.vanillafix.bugs.mixins.client.MinecraftClientMixin":
-                return config().clientOnly.splitScreenAndTextureProfiler;
-            case "org.dimdev.vanillafix.bugs.mixins.client.ClientPlayerEntityMixin":
-                return config().clientOnly.screenInNetherPortal;
-            case "org.dimdev.vanillafix.bugs.mixins.client.ClientPlayNetworkHandlerMixin":
-                return config().clientOnly.fastInterdimensionalTeleportation;
-            case "org.dimdev.vanillafix.bugs.mixins.BuiltinBiomesMixin":
-                return config().bugFixes.fixStoneShoreColors;
-            case "org.dimdev.vanillafix.bugs.mixins.SignBlockEntityMixin":
-                return config().bugFixes.fixSignCommands;
-        }
-        return true;
-    }
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
 
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-    }
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
 
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-    }
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
 }
